313024687634d185bf1f5ed52b79d6fcee4097da
tarrciodev <tarcisio.devmaster@gmail.com>
2025-05-02 11:11:57 +0100

    feat: app using promise all to return search and pagination

--- src/services/projects/get-projects-service.ts
+++ src/services/projects/get-projects-service.ts
@@ -11,83 +11,85 @@ interface IGetProjectProps {
 export async function getProjectsService({ query }: IGetProjectProps) {
   const { page, take, slug } = query
   const skip = (page - 1) * take
-  const projects = await prisma.project.findMany({
-    where: {
-      category: {
-        slug: {
-          contains: slug,
+
+  const [projects, totalItems] = await Promise.all([
+    prisma.project.findMany({
+      where: {
+        category: {
+          slug: {
+            contains: slug,
+          },
+        },
+        ownerId: {
+          not: query.me,
         },
       },
-      ownerId: {
-        not: query.me,
-      },
-    },
-    select: {
-      id: true,
-      name: true,
-      description: true,
-      createdAt: true,
-      status: true,
-      subcategoryId: true,
-      subscriptions: {
-        select: {
-          id: true,
-          toliner: {
-            select: {
-              id: true,
-              name: true,
-              user: {
-                select: {
-                  avatarUrl: true,
+      select: {
+        id: true,
+        name: true,
+        description: true,
+        createdAt: true,
+        status: true,
+        subcategoryId: true,
+        subscriptions: {
+          select: {
+            id: true,
+            toliner: {
+              select: {
+                id: true,
+                name: true,
+                user: {
+                  select: {
+                    avatarUrl: true,
+                  },
                 },
               },
             },
+            createdAt: true,
           },
-          createdAt: true,
         },
-      },
-      skills: {
-        select: {
-          id: true,
-          name: true,
+        skills: {
+          select: {
+            id: true,
+            name: true,
+          },
         },
-      },
-      category: {
-        select: {
-          id: true,
-          name: true,
-          subcategories: {
-            select: {
-              id: true,
-              name: true,
+        category: {
+          select: {
+            id: true,
+            name: true,
+            subcategories: {
+              select: {
+                id: true,
+                name: true,
+              },
             },
           },
         },
-      },
-      owner: {
-        select: {
-          id: true,
-          name: true,
+        owner: {
+          select: {
+            id: true,
+            name: true,
+          },
         },
       },
-    },
-    skip,
-    take,
-    orderBy: {
-      createdAt: 'desc',
-    },
-    cacheStrategy: { ttl: 60 },
-  })
-
-  const totalItems = await prisma.project.count({
-    where: {
-      category: {
-        slug: {
-          contains: slug,
+      skip,
+      take,
+      orderBy: {
+        createdAt: 'desc',
+      },
+      cacheStrategy: { ttl: 60 },
+    }),
+    prisma.project.count({
+      where: {
+        category: {
+          slug: {
+            contains: slug,
+          },
         },
       },
-    },
-  })
+    }),
+  ])
 
   const parseProjects = projects.map(project => {
     const { subcategoryId, ...rest } = project
