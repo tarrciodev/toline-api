42540a13f3fda2df8a37f322915cc60df5a4b13c
tarrciodev <tarcisio.devmaster@gmail.com>
2025-05-15 09:59:51 +0100

    fix: charge does not reflect anymore if rejected

--- src/admin/services/transactions/verify-charge-admin-service.ts
+++ src/admin/services/transactions/verify-charge-admin-service.ts
@@ -1,46 +1,53 @@
-import { prisma } from '../../../config/prisma'
+import { prisma } from "../../../config/prisma";
 
 export async function verifyChargeAdminService({
-  id,
-  status,
-}: { id: string; status: string }) {
-  const charge = await prisma.charge.update({
-    where: {
-      id,
-    },
-    data: {
-      status: status as 'rejected' | 'resolved',
-    },
-    select: {
-      id: true,
-      ammount: true,
-      toliner: {
-        select: {
-          id: true,
-          balance: true,
+    id,
+    status,
+}: {
+    id: string;
+    status: string;
+}) {
+    const charge = await prisma.charge.update({
+        where: {
+            id,
         },
-      },
-    },
-  })
-
-  await prisma.toliner.update({
-    where: {
-      id: charge?.toliner?.id,
-    },
-    data: {
-      balance: charge?.toliner?.balance
-        ? {
-            update: {
-              ammount: charge?.toliner?.balance.ammount + charge.ammount,
+        data: {
+            status: status as "rejected" | "resolved",
+        },
+        select: {
+            id: true,
+            ammount: true,
+            toliner: {
+                select: {
+                    id: true,
+                    balance: true,
+                },
             },
-          }
-        : {
-            create: {
-              ammount: charge.ammount,
-            },
-          },
-    },
-  })
+        },
+    });
 
-  return charge
+    if (status === "resolved") {
+        await prisma.toliner.update({
+            where: {
+                id: charge?.toliner?.id,
+            },
+            data: {
+                balance: charge?.toliner?.balance
+                    ? {
+                          update: {
+                              ammount:
+                                  charge?.toliner?.balance.ammount +
+                                  charge.ammount,
+                          },
+                      }
+                    : {
+                          create: {
+                              ammount: charge.ammount,
+                          },
+                      },
+            },
+        });
+    }
+
+    return charge;
 }
